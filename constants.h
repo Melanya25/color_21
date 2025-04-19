86a02e21fac7645275b541e606ee4a9a59c148e9
Melanya25 <o.melanya@mail.ru>
2025-04-19 13:01:39 +0300

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define DIR_TEMPLATES "./templates/"
+#define DIR_CSS      "./css/"
+
+#define PATH_LOGIN_HTML    DIR_TEMPLATES "login.html"
+#define PATH_SUCCESS_HTML  DIR_TEMPLATES "success.html"
+#define PATH_ERROR_HTML    DIR_TEMPLATES "error.html"
+#define PATH_COLOR_HTML    DIR_TEMPLATES "color.html"
+#define PATH_CSS_STYLES    DIR_CSS "styles.css"
+
+#define CONTENT_TYPE_HTML  "Content-Type: text/html; charset=utf-8\n"
+#define CONTENT_TYPE_CSS   "Content-Type: text/css; charset=utf-8\n"
+
+#endif
