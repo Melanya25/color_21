0917c036417d3c9f1bab4daeff761860c5f75c4b
Melanya25 <o.melanya@mail.ru>
2025-04-19 16:06:15 +0300

    Update constants.h

--- constants.h
+++ constants.h
@@ -2,15 +2,15 @@
 #define CONSTANTS_H
 
 #define DIR_TEMPLATES "./templates/"
-#define DIR_CSS "./css/"
+#define DIR_CSS       "./css/"
 
-#define PATH_LOGIN_HTML DIR_TEMPLATES "login.html"
-#define PATH_SUCCESS_HTML DIR_TEMPLATES "success.html"
-#define PATH_ERROR_HTML DIR_TEMPLATES "error.html"
-#define PATH_COLOR_HTML DIR_TEMPLATES "color.html"
-#define PATH_CSS_STYLES DIR_CSS "styles.css"
+#define PATH_LOGIN_HTML    DIR_TEMPLATES "login.html"
+#define PATH_SUCCESS_HTML  DIR_TEMPLATES "success.html"
+#define PATH_ERROR_HTML    DIR_TEMPLATES "error.html"
+#define PATH_COLOR_HTML    DIR_TEMPLATES "color.html"  // [NEW]
+#define PATH_CSS_STYLES    DIR_CSS "styles.css"
 
-#define CONTENT_TYPE_HTML "Content-Type: text/html; charset=utf-8\n"
-#define CONTENT_TYPE_CSS "Content-Type: text/css; charset=utf-8\n"
+#define CONTENT_TYPE_HTML  "Content-Type: text/html; charset=utf-8\n"
+#define CONTENT_TYPE_CSS   "Content-Type: text/css; charset=utf-8\n"
 
 #endif
