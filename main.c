bb7c0bb38455c05a3b21c21cb2741415dcd10341
Melanya25 <o.melanya@mail.ru>
2025-04-19 16:07:30 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,76 +1,52 @@
-#include "mongoose.h"
+#include "../mongoose/mongoose.h"
 #include "input.h"
 #include "constants.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-enum {
-    ERR_OK = 0,
-    ERR_MISSING_ENV = 1,
-    ERR_INVALID_CREDENTIALS = 2,
-    ERR_FILE_NOT_FOUND = 3
-};
-
-static int process_request(struct mg_connection *c, struct mg_http_message *hm) {
-    int status_code = 500;
-    const char *ctype = "";
-    char *response = NULL;
-    int error_code = ERR_FILE_NOT_FOUND;
-
-    if (mg_strcmp(hm->url, mg_str("/login")) == 0 && mg_strcasecmp(hm->method, mg_str("POST")) == 0) {
-        // Обработка авторизации (как в оригинале)
-    } 
-    else if (mg_strcmp(hm->url, mg_str("/color")) == 0 && mg_strcasecmp(hm->method, mg_str("POST")) == 0) {
-        // Обработка формы цвета
-        char bg_red[4], bg_green[4], bg_blue[4], text_red[4], text_green[4], text_blue[4], content[256];
-        
-        mg_http_get_var(&hm->body, "bg_red", bg_red, sizeof(bg_red));
-        mg_http_get_var(&hm->body, "bg_green", bg_green, sizeof(bg_green));
-        mg_http_get_var(&hm->body, "bg_blue", bg_blue, sizeof(bg_blue));
-        mg_http_get_var(&hm->body, "text_red", text_red, sizeof(text_red));
-        mg_http_get_var(&hm->body, "text_green", text_green, sizeof(text_green));
-        mg_http_get_var(&hm->body, "text_blue", text_blue, sizeof(text_blue));
-        mg_http_get_var(&hm->body, "content", content, sizeof(content));
-
-        char *color_template = read_file(PATH_COLOR_HTML);
-        if (color_template) {
-            char *dynamic_response = malloc(strlen(color_template) + 512);
-            sprintf(dynamic_response, color_template, 
-                    bg_red, bg_green, bg_blue,
-                    text_red, text_green, text_blue,
-                    content);
-            
-            status_code = 200;
-            ctype = CONTENT_TYPE_HTML;
-            error_code = ERR_OK;
-            response = dynamic_response;
-            free(color_template);
-        }
-    }
-    else if (mg_strcmp(hm->url, mg_str("/styles.css")) == 0) {
-        response = read_file(PATH_CSS_STYLES);
-        if (response) {
-            status_code = 200;
-            ctype = CONTENT_TYPE_CSS;
-            error_code = ERR_OK;
-        }
-    } else {
-        response = read_file(PATH_LOGIN_HTML);
-        if (response) {
-            status_code = 200;
-            ctype = CONTENT_TYPE_HTML;
-            error_code = ERR_OK;
-        }
-    }
-
-    if (error_code == ERR_OK) {
-        mg_http_reply(c, status_code, ctype, "%s", response);
-    } else {
-        mg_http_reply(c, 500, "", "", "");
-    }
-
-    free(response);
-    return error_code;
+// [NEW] Color response generator
+static char *generate_color_response(const char *text, 
+                                   const char *bg_red, const char *bg_green, const char *bg_blue,
+                                   const char *text_red, const char *text_green, const char *text_blue) {
+    char *html = malloc(1024);
+    snprintf(html, 1024,
+        "<!DOCTYPE html><html><head><title>Color Result</title>"
+        "<style>body{background-color:rgb(%s,%s,%s);color:rgb(%s,%s,%s);"
+        "display:flex;justify-content:center;align-items:center;height:100vh;"
+        "font-size:2em;}</style></head><body>%s</body></html>",
+        bg_red, bg_green, bg_blue, text_red, text_green, text_blue, text);
+    return html;
 }
 
-// Остальной код как в оригинале...
+static int process_request(struct mg_connection *c, struct mg_http_message *hm) {
+    // ... существующий код авторизации ...
+
+    // [NEW] Color configuration handler
+    else if (mg_strcmp(hm->url, mg_str("/color")) == 0) {
+        if (mg_strcasecmp(hm->method, mg_str("POST")) == 0) {
+            char text[100], bg_red[4], bg_green[4], bg_blue[4];
+            char text_red[4], text_green[4], text_blue[4];
+            
+            mg_http_get_var(&hm->body, "text", text, sizeof(text));
+            mg_http_get_var(&hm->body, "bg_red", bg_red, sizeof(bg_red));
+            // ... аналогично для других полей ...
+            
+            char *response = generate_color_response(text, bg_red, bg_green, bg_blue,
+                                                   text_red, text_green, text_blue);
+            mg_http_reply(c, 200, CONTENT_TYPE_HTML, "%s", response);
+            free(response);
+            return ERR_OK;
+        } else {
+            // GET request - show color form
+            char *response = read_file(PATH_COLOR_HTML);
+            if (response) {
+                mg_http_reply(c, 200, CONTENT_TYPE_HTML, "%s", response);
+                free(response);
+                return ERR_OK;
+            }
+        }
+    }
+    
+    // ... остальной существующий код ...
+}
