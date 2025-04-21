6c46e7a5df29e30458e4f997ae9f003b47af6098
Melanya25 <o.melanya@mail.ru>
2025-04-21 11:45:19 +0300

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,7 +1,14 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define PATH_COLORS_HTML "colors.html"
-#define PATH_RESULT_HTML "result.html"
+#define DIR_TEMPLATES "./templates/"
+#define DIR_CSS       "./css/"
+
+#define PATH_COLOR_FORM_HTML   DIR_TEMPLATES "color_form.html"
+#define PATH_COLOR_RESULT_HTML DIR_TEMPLATES "color_result.html"
+#define PATH_CSS_STYLES      DIR_CSS       "styles.css"
+
+#define CONTENT_TYPE_HTML "Content-Type: text/html; charset=utf-8\r\n"
+#define CONTENT_TYPE_CSS  "Content-Type: text/css; charset=utf-8\r\n"
 
 #endif
